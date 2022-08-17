9d99709e3793950bba4dedfa918b001a26b281ae
vedpathakp127 <p.vedpathak@elsevier.com>
2022-08-17 13:28:05 +0530

    fix

--- Jenkins_Job_Folder/firstJob.sh
+++ Jenkins_Job_Folder/firstJob.sh
@@ -1,3 +1,5 @@
+#!/bin/sh
+
 final String url = "http://localhost:8080/job/Demos/job/maven-pipeline-demo/job/sdkman/2/api/json"
 
 final String response = sh(script: "curl -s $url", returnStdout: true).trim()
