5b289c82d9dae5f4439f13d74e950c10eccad747
vedpathakp127 <p.vedpathak@elsevier.com>
2022-08-17 09:46:20 +0530

    added shell scripts

---
+++ Jenkins_Job_Folder/firstJob.sh
@@ -0,0 +1,5 @@
+final String url = "http://localhost:8080/job/Demos/job/maven-pipeline-demo/job/sdkman/2/api/json"
+
+final String response = sh(script: "curl -s $url", returnStdout: true).trim()
+
+echo response
